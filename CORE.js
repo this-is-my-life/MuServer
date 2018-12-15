e59d0a398b30c49fd02072dbf305a5fbfdeb7955
PMH <kok4575@gmail.com>
2018-12-15 17:32:26 +0900

    [12/15] Hot Fixed

--- CORE.js
+++ CORE.js
@@ -215,40 +215,40 @@ mu.get('/action/Dobak/:id/:verify', (req, res) => {
         // 아래 if문들은 위에서 부터 계산이 중요한 순서대로 정렬되야 정상적으로 뮤트코인이 지급됩니다
         if (Slot1 == Slot2 && Slot2 == Slot3 && Slot1 == ":seven:") {
             SlotResult = "끼이야야야야아악!! 777!!! 잭팟이다뮤!!!!!! [기존 보유량에 777배]";
-            mute[id] = {
-                mute: mute[id].mute * 777
+            UsersCoin[id] = {
+                UsersCoin: UsersCoin[id].UsersCoin * 777
             };
-            cmds.writeFile("../Saved/UsersCoin.json", JSON.stringify(mute), (error) => { if (error) { } });
+            cmds.writeFile("./Saved/UsersCoin.json", JSON.stringify(UsersCoin), (error) => { if (error) { } });
         } else if (Slot1 == Slot2 && Slot2 == Slot3 && Slot1 == ":zero:") {
             SlotResult = "엌ㅋ 000 실화얔ㅋ [기존 보유량에 0배, ㅅㄱ]";
-            mute[id] = {
-                mute: mute[id].mute * 0
+            UsersCoin[id] = {
+                UsersCoin: UsersCoin[id].UsersCoin * 0
             };
-            cmds.writeFile("../Saved/UsersCoin.json", JSON.stringify(mute), (error) => { if (error) { } });
+            cmds.writeFile("./Saved/UsersCoin.json", JSON.stringify(UsersCoin), (error) => { if (error) { } });
         } else if (Slot1 == Slot2 && Slot2 == Slot3) {
             SlotResult = "3슬롯을 맞추는데 성공했다뮤! [+8192 MUC]";
-            mute[id] = {
-                mute: mute[id].mute + 8192
+            UsersCoin[id] = {
+                UsersCoin: UsersCoin[id].UsersCoin + 8192
             };
-            cmds.writeFile("../Saved/UsersCoin.json", JSON.stringify(mute), (error) => { if (error) { } });
+            cmds.writeFile("./Saved/UsersCoin.json", JSON.stringify(UsersCoin), (error) => { if (error) { } });
         } else if (Slot1 == Slot2 || Slot2 == Slot3) {
             SlotResult = "2슬롯을 연속으로 맞추는데 성공했다뮤! [+2048 MUC]";
-            mute[id] = {
-                mute: mute[id].mute + 2048
+            UsersCoin[id] = {
+                UsersCoin: UsersCoin[id].UsersCoin + 2048
             };
-            cmds.writeFile("../Saved/UsersCoin.json", JSON.stringify(mute), (error) => { if (error) { } });
+            cmds.writeFile("./Saved/UsersCoin.json", JSON.stringify(UsersCoin), (error) => { if (error) { } });
         } else if (Slot1 == Slot3) {
             SlotResult = "연속으론 아니지만 2슬롯을 맞추는데 성공했다뮤! [+1024MUC]";
-            mute[id] = {
-                mute: mute[id].mute + 1024
+            UsersCoin[id] = {
+                UsersCoin: UsersCoin[id].UsersCoin + 1024
             };
-            cmds.writeFile("../Saved/UsersCoin.json", JSON.stringify(mute), (error) => { if (error) { } });
+            cmds.writeFile("./Saved/UsersCoin.json", JSON.stringify(UsersCoin), (error) => { if (error) { } });
         } else {
             SlotResult = "히익, 운이 없네뮤... [-10MUC(도박비용)]";
-            mute[id] = {
-                mute: mute[id].mute - 10
+            UsersCoin[id] = {
+                UsersCoin: UsersCoin[id].UsersCoin - 10
             };
-            cmds.writeFile("../Saved/UsersCoin.json", JSON.stringify(mute), (error) => { if (error) { } });
+            cmds.writeFile("./Saved/UsersCoin.json", JSON.stringify(UsersCoin), (error) => { if (error) { } });
         }
             return res.status(200).json({"status": "OK", "Slot1": Slot1, "Slot2": Slot2, "Slot3": Slot3 , "SlotResult": SlotResult});
 		}
