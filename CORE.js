16fc36c1ae286da30e4063a4a514df18e4e4341d
PMH <kok4575@gmail.com>
2018-12-15 16:57:58 +0900

    [12/15] Only Get Command

--- CORE.js
+++ CORE.js
@@ -8,9 +8,15 @@
 */
 
 const server = require("express");
+const bodyParser = require('body-parser');
 const UsersCoin = require("./Saved/UsersCoin.json");
 const ServersPrefix = require("./Saved/ServersPrefix.json");
+const cmds = require("fs");
 const mu = server();
+
+mu.use(bodyParser.json());
+mu.use(bodyParser.urlencoded({ extended: true }));
+
 
 let DBport = 80;
 
@@ -26,7 +32,7 @@ mu.get("/", (req, res) => {
         "have": ["UsersCoin.json", "ServersPrefix.json", "AV.avi"],
         "canGive": ["UsersCoin.json", "ServersPrefix.json"],
         "cannotGive": ["AV.avi"],
-        "howToGet": "/<FileName>"
+        "howToGet": "/api/<FileName>"
     });
 });
 
@@ -38,16 +44,88 @@ mu.get('/api/UsersCoin.json/:id', (req, res) => {
     if (!id) {
       return res.status(400).json({error: 'Incorrect id'});
     }
-  
-    let user = UsersCoin.filter(user => user.userID === id)[0]
-    if (!user) {
-      return res.status(404).json({error: 'Unknown user'});
+    let userCoinData = UsersCoin[id].UsersCoin;
+    if (!userCoinData) {
+        return res.status(404).json({error: 'Not Found That User'})
     }
-  
-    return res.json(user);
+    return res.json({
+        "UserID": id,
+        "mute": userCoinData
+    });
   });
   
-  
+mu.get('/action/UserTyped/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (!id) {
+      return res.status(400).json({error: 'Incorrect id'});
+    } else {
+    // MuteCoin
+		{
+			if (!UsersCoin[id]) {
+				UsersCoin[id] = {
+					UsersCoin: 0
+				};
+			}
+			let muteAmt = Math.floor(Math.random() * 11) + 1;
+			if (muteAmt == 1) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 1
+				};
+			} else
+			if (muteAmt == 2) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 2
+				};
+			} else
+			if (muteAmt == 3) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 4
+				};
+			} else
+			if (muteAmt == 4) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 8
+				};
+			} else
+			if (muteAmt == 5) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 16
+				};
+			} else
+			if (muteAmt == 6) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 32
+				};
+			} else		
+			if (muteAmt == 7) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 64
+				};
+			} else
+			if (muteAmt == 8) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 128
+				};
+			} else
+			if (muteAmt == 9) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 256
+				};
+			} else
+			if (muteAmt == 10) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 512
+				};
+			} else
+			if (muteAmt == 11) {
+				UsersCoin[id] = {
+					UsersCoin: UsersCoin[id].UsersCoin + 1024
+				};
+			}
+			cmds.writeFile("./Saved/UsersCoin.json", JSON.stringify(UsersCoin), (error) => { if (error) { console.log(error); } });
+		}
+    }
+});
 
 // Servers Prefix
 mu.get("/api/ServersPrefilx.json", (req, res) => res.json(ServersPrefix));
